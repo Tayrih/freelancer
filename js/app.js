ee814b9955ab08282f7b6fb551c431c3d6f3a11b
Tayrih <tahysluana@gmail.com>
2017-11-13 23:06:06 -0500

    Añade modal a la imagen 1, con error de eventos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-function mostrarVentana()
+function mostrarImg()
 {
-    var ventana = document.getElementByClassName('modal');
-    ventana.style.marginTop = "100px";
-    ventana.style.display = 'block';
+    var ventana = document.getElementById('image1');
+    ventana.style.visibility = "visible";
 }
+
+function modal() {
+  var img = document.getElementsByClassName('image');
+  for (var i = 0; i < img.length; i++) {
+    img[i] =  document.getElementsByClassName('image')[i];
+    img[i].addEventListener("click", mostrarImg);
+  }
+}
+
+window.onload = modal;
